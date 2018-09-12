db544d8f568557a6d0e1804841787f3b8422d9bb
Tom Schof <tom-schof@users.noreply.github.com>
2018-09-12 09:48:22 -0400

    files set

---
+++ liri.js
@@ -0,0 +1,74 @@
+require("dotenv").config();
+
+var request = require("request");
+var moment = require('moment');
+var Spotify = require('node-spotify-api');
+var bandsintown = require('bandsintown')("codingbootcamp");
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+moment().format();
+
+
+
+ 
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
+
+
+
+
+var nodeArgs = process.argv;
+
+// Create an empty variable for holding the movie name
+var movieName = "";
+
+// Loop through all the words in the node argument
+// And do a little for-loop magic to handle the inclusion of "+"s
+for (var i = 2; i < nodeArgs.length; i++) {
+
+  if (i > 2 && i < nodeArgs.length) {
+
+    movieName = movieName + "+" + nodeArgs[i];
+
+  }
+
+  else {
+
+    movieName += nodeArgs[i];
+
+  }
+}
+
+// Then run a request to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+request(queryUrl, function(error, response, body) {
+
+  // If the request is successful
+  if (!error && response.statusCode === 200) {
+
+    // Parse the body of the site and recover just the imdbRating
+    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    console.log("Release Year: " + JSON.parse(body).Year);
+  }
+});
+
+
+
+ 
+bandsintown
+  .getArtist('Skrillex')
+  .then(function(events) {
+      console.log(events);
+    // return array of events
+  });
